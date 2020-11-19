eb8cafdb6b19408007315a1ea10673a96a911199
shreyashravi <shreyash.ravi@gmail.com>
2020-11-19 15:02:30 +0530

    fixed player names

--- players/__init__.py
+++ players/__init__.py
@@ -2,7 +2,7 @@ from .base_player import _init as _init_base
 from .simpleton import Simpleton, _init as _init_simpleton
 from .TypeR import TypeR, _init as _init_TypeR
 from .TypeRi import TypeRi, _init as _init_TypeRi
-from .typeShr import TypeShr, _init as _init_TypeShr
+from .TypeShr import TypeShr, _init as _init_TypeShr
 from .TypePapa import TypePapa, _init as _init_TypePapa
 
 
