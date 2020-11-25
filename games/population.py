1d9d49e7f20a732a350ba642e73d2d833ae686f9
shreyashravi <shreyash.ravi@gmail.com>
2020-11-25 19:21:31 +0530

    Group scenario updates

--- games/population.py
+++ games/population.py
@@ -1,6 +1,6 @@
 import logging
 from json import load
-from typing import List
+from typing import List, Tuple, Dict
 
 from enviornments import BaseEnvironment
 
@@ -20,6 +20,9 @@ class Population:
     pops: List[float]
     params: List[List[float]]
     params_order: List[str]
+    fluctuation: float
+
+    coward_data: Dict
 
     def __init__(self, env, data_file=None):
         self.env = env
@@ -29,10 +32,14 @@ class Population:
         with open(data_file) as f:
             raw_data = load(f)
 
+        self.fluctuation = raw_data['p-healthy-fluctuation']
         self.sections = []
         self.pops = []
         self.params = []
         self.params_order = raw_data['jobs-param-order'] + ["danger"]
+
+        self.coward_data = raw_data["player-types"]["coward"]
+
 
         for k1, v1 in raw_data["jobs"].items():
             for k2, v2 in raw_data["population"].items():
@@ -42,15 +49,28 @@ class Population:
 
     def coward_action(self, idx, last_w, i_status):
         """
-        :param idx:      The index of the section for which this function is called {0, ..., 15}
+        :param idx:      The index of the section for which this function is called {0, ..., 15}[UNUSED]
         :type idx:       int
 
-        :param last_w:   The ratio of people (of this type) who went to work yesterday
-        :type last_w:    float
+        :param last_w:   The ratio of people (of this type) who went to work yesterday for each day
+                         of infection
+                         e.g. idx = 0, is the ratio (young primary coward sus W(t-1) / young primary cowards sus)
+        :type last_w:    List[float]
 
-        :param i_status: Ratios for the day of infection this is (-1 -> S, t_recovery -> R)
+        :param i_status: Ratios for the day of infection this is (0 -> S, t_recovery -> R)
+                         e.g. idx = 0, is the ratio (young primary coward sus / young primary cowards)
         :type i_status:  List[float]
 
         :return: ratio of people who will choose to work
         :rtype:  float
         """
+        w = []
+        threshold_sw = self.coward_data['w-threshold']
+        threshold_sh = self.coward_data['h-threshold']
+
+        ratio_over_threshold_w = (threshold_sw - (1 - fluctuation)) / (2 * fluctuation)
+        ratio_over_threshold_h = (threshold_sh - (1 - fluctuation)) / (2 * fluctuation)
+
+        w.append(last_w[0] * ratio_over_threshold_w + (1 - last_w[0]) * ratio_over_threshold_h)
+
+        return w
--- players/base_player.py
+++ players/base_player.py
@@ -106,7 +106,8 @@ class BasePlayer:
             self.p_healthy = 1
         elif self.state == "I":
             self.p_healthy = 1 - (self.env.t - self.t_i) / self.env.TIMES['infectious']
-        self.p_healthy += np.random.rand() / 4 - 0.125
+        fluctuation = 0.25
+        self.p_healthy += (np.random.rand() - 0.5) * fluctuation
         self.p_healthy = min(1.0, max(0.0, self.p_healthy))
 
     @property
